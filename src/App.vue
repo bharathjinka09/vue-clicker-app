8dbae5356e0d50c37d556579fb342a7639e6e3d8
bharathjinka09 <bharathjinka09@gmail.com>
2020-12-05 15:44:33 +0530

    Clicker app done!

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Clicker />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Clicker from './components/Clicker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Clicker
   }
 }
 </script>
---
+++ src/components/Clicker.vue
@@ -0,0 +1,38 @@
+<template>
+<h1>{{title}}</h1>
+<h1>{{count}}</h1>
+<button class="btn btn-info m-2" @click="increment">Increment</button>
+<button class="btn btn-info m-2" @click="decrement">Decrement</button>
+</template>   
+
+<script>
+import {ref} from 'vue';
+
+export default {
+    setup(){
+        let title = "Clicker App";
+
+        const count = ref(0);
+
+        function increment(){
+            count.value++;
+        }
+
+        function decrement(){
+           count.value > 0 ? count.value-- : '';
+        }
+
+        return {
+            title,
+            count,
+            increment,
+            decrement,
+        }
+    }
+}
+</script> 
+
+
+<style scoped>
+
+</style>
